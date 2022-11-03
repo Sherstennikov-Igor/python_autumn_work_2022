dc2ec1ebbf69a4d165fb40c5aedcce0782596d79
Igor_Sherstennikov <sherstennikov.igor@yandex.ru>
2022-11-04 00:34:17 +0300

    refactor 32

--- unit_two/task_32.py
+++ unit_two/task_32.py
@@ -10,20 +10,22 @@
 #  в момент вычитания объектов класса obj1 - obj2 и заключается в вычитании из
 #  здоровья первого объекта урона наносимого вторым объектом
 from random import randint as rand
+from random import choices
 
 
 class Char:
     # counter = 0
-    def __init__(self, name, lvl, char_type, race):
+    def __init__(self, name, lvl, char_type, race, dodge):
         self.id = rand(0, 100500)
         self.name = name
         self.lvl = lvl
         self.max_hp = rand(50, 150) * lvl
         self.hp = self.max_hp
-        self.base_dmg = rand(1, 2) * lvl
+        self.base_dmg = rand(1, 1) * lvl
         self.char_type = char_type
         self.race = race
         self.xp_gen = self.hp
+        self.dodge = dodge
         # Char.counter += 1
 
     def kill(self, dmg):
@@ -43,17 +45,28 @@ class Char:
             case "Anumal":
                 dmg = self.base_dmg*rand(1, 2)
             case "LoL":
-                dmg = self.base_dmg*rand(1, 100)
+                dmg = self.base_dmg*rand(-100, 100)
         return dmg
 
     def __sub__(self, other):
-        print(f"{self.name} наносит {self.dmg_range(self.char_type)} урона по {other.name}")
-        other.kill(self.dmg_range(self.char_type))
+        hit_dmg = self.dmg_range(self.char_type)
+        dodge = choices([0, 1], weights=[1 - other.dodge / 100, other.dodge / 100])[0]
+        if dodge:
+            print(f"{self.name} нытается нанести удар по {other.name}\n"
+                  f"Но подлый {other.name} увернулся от удара")
+        else:
+            print(f"{self.name} наносит {hit_dmg} урона по {other.name}")
+            other.kill(hit_dmg)
         if other.hp > 0:
             print(f"У {other.name} осталось {other.hp} здоровья из {other.max_hp}")
-            print(f"{other.name} наносит {other.base_dmg} урона по {self.name}")
-            self.kill(other.base_dmg)
-            print(f"У {self.name} осталось {self.hp} здоровья из {self.max_hp}")
+            hit_dmg = other.dmg_range(other.char_type)
+            dodge = choices([0, 1], weights=[1 - self.dodge / 100, self.dodge / 100])[0]
+            if dodge:
+                print(f"Но храбрый {self.name} увернулся от удара")
+            else:
+                self.kill(hit_dmg)
+                print(f"{other.name} наносит {hit_dmg} урона по {self.name}\n"
+                      f"У {self.name} осталось {self.hp} здоровья из {self.max_hp}")
 
     def __repr__(self):
         return f'''
@@ -71,12 +84,12 @@ class Battle:
 
     def battle(self):
         while self.char_1.hp > 0 and self.char_2.hp > 0:
-            hero - enemy_1
+            self.char_1 - self.char_2
 
 
-hero = Char("Ivan", 2, "Warrior", "Human")
-enemy_1 = Char("Rat", 1, "Anumal", "Neutral")
-enemy_2 = Char("Elephant", 1, "Anumal", "Neutral")
+hero = Char("Ivan", 2, "Warrior", "Human", 50)
+enemy_1 = Char("Rat", 1, "Anumal", "Neutral", 30)
+enemy_2 = Char("Elephant", 4, "Anumal", "Neutral", 0)
 
-fight = Battle(hero, enemy_1)
+fight = Battle(hero, enemy_2)
 fight.battle()
