1eb48d2ef28c0f561ff931a3991740d707bc06a1
Igor_Sherstennikov <sherstennikov.igor@yandex.ru>
2022-12-02 23:20:17 +0300

    Diplom v 0.2.2

--- Diplom/get_IFC.py
+++ Diplom/get_IFC.py
@@ -8,32 +8,33 @@ def insert_element(ifc_file):
         cursor.execute(f'''INSERT INTO element(id_guid_element, class_ifc)
         VALUES('{element.get_info()["GlobalId"]}', '{element.is_a()}')''')
 
-def open_ifc(path='.'):
-    ''' Функция пакетной обработки IFC файлов'''
-    files = sorted(os.listdir(path))
-    for file in files:
-        time_file = time()
-        if file.endswith(".ifc"):
-            print("Обработка файла", file)
-            ifc_file = ifcopenshell.open(file)
-            insert_element(ifc_file)
-            print("\t\t\t\tЗатрачено времени", (time() - time_file))
 
 
+files = sorted(os.listdir('.'))
+for file in files:
+    time_file = time()
+    if file.endswith(".ifc"):
+        print("Обработка файла", file)
+        ifc_file = ifcopenshell.open(file)
+        element = ifc_file.by_type("IfcBuildingElement")
+        propertry_set = ifc_file.by_type("IFCPROPERTYSET")
+        propertry = ifc_file.by_type("IFCPROPERTYSINGLEVALUE")
 
 
-# ifc_file = open_ifc()
-#
-#
-# element = ifc_file.by_type("IfcBuildingElement")
-# propertry_set = ifc_file.by_type("IFCPROPERTYSET")
-#
-# for i in propertry_set:
-#     ss = i.get_info()
-#     # print(ss)
-#     print(ss["GlobalId"])
-#     print(ss["Name"])
-#     print(ss["HasProperties"])
-#     # print(i.get_info(recursive=True))
-#
-# # print(ifc_file.by_id(823))
+        #
+        # print(ifc_file.by_id(341))
+
+
+for i in element:
+    print(i.get_info()["GlobalId"])
+    ss = ifcopenshell.util.element.get_psets(i)
+    print(ss)
+    for pset, propertys in ss.items():
+        # print(pset)
+        for property, var in propertys.items():
+            if property == "id":
+                print(ifc_file.by_id(var).get_info()["GlobalId"], pset)
+
+            else:
+                print(property, var)
+
